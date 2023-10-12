9c0ec737a9d326fc368520d89277bb5a62f840e9
Inigo de Robina <iderobina@MacBook-Air-2.lan>
2023-10-12 11:10:42 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -13,26 +13,43 @@ User.init(
     {
 
 id: {
-
-
-
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
 },
 
 username: {
-
-
-
+    type: DataTypes.STRING,
+    allowNull: false,
 },
 
 
 password: {
-
-
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+        len: [8,10],
+        isStrongPassword(value) {
+        if (!/[A-Z]/.test(value) || 
+            !/[a-z]/.test(value) ||
+            !/[0-9]/.test(value) ||
+            !/[@!#\$%^&*]/.test(value)) {
+            throw new Error('Password does not meet complexity requirements.');
+            }
+            }
+        }
+    },
 
 },
 
-
+{
+    hooks:
+    as
 
 }
+
+
+
 
 )
