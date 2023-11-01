a3f5c144911701177912a33b731ce754b4d85fc9
Inigo de Robina <iderobina@MacBook-Air-2.local>
2023-10-31 18:01:53 -0600

    signup reroute

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -14,7 +14,6 @@ async function signupFormHandler(event) {
       headers: { 'Content-Type': 'application/json' }
     });
 
-    // check the response status
     if (response.ok) {
       const response = await fetch('/api/users/login', {
         method: 'post',
@@ -25,7 +24,6 @@ async function signupFormHandler(event) {
         headers: { 'Content-Type': 'application/json' }
       });
 
-      // check the response status
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sess));
 
-// turn on routes
 app.use(routes);
 
 app.engine('handlebars', hbs.engine);
